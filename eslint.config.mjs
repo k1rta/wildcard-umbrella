b67eaf54bfa7aaa14ca550f1520e19a5e7f8c1d6
Kirta-Linda Karits <31627919+k1rta@users.noreply.github.com>
2025-12-09 15:41:09 +0200

    chore: Clean up workflow and fix linting issues

---
+++ eslint.config.mjs
@@ -0,0 +1,51 @@
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
+import { FlatCompat } from '@eslint/eslintrc'
+import nextPlugin from '@next/eslint-plugin-next'
+import reactPlugin from 'eslint-plugin-react'
+import typescriptEslint from '@typescript-eslint/eslint-plugin'
+import tsParser from '@typescript-eslint/parser'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+})
+
+export default [
+  {
+    plugins: {
+      '@typescript-eslint': typescriptEslint,
+      '@next/next': nextPlugin,
+      react: reactPlugin,
+    },
+
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+
+    rules: {
+      'react/display-name': 'off',
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
+        {
+          argsIgnorePattern: '^_',
+        },
+      ],
+      'no-unused-vars': 'off',
+    },
+
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+  },
+]
