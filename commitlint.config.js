22d1e7d70c23a336a6b4ad5fe90ae74c85fff7f2
Kirta-Linda Karits <31627919+k1rta@users.noreply.github.com>
2025-12-11 09:48:40 +0200

    fix: Update build configuration
    - Remove custom Babel config to use Next.js SWC
    - Update PostCSS and Next.js configs to use CommonJS
    - Fix module resolution issues

---
+++ commitlint.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'subject-case': [2, 'always', 'sentence-case'],
+    'header-max-length': [2, 'always', 100],
+    'body-max-line-length': [2, 'always', 100],
+  },
+}
