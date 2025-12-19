6c3cd8b8655e26b4fb5dfc10e97e2ab32bd37eb1
Kirta-Linda Karits <31627919+k1rta@users.noreply.github.com>
2025-12-19 14:07:17 +0200

    fix: Update lint-staged config to use CommonJS syntax

--- lint-staged.config.cjs
+++ lint-staged.config.cjs
@@ -1,10 +1,10 @@
-export default {
+module.exports = {
   // Format all files
   '*': 'prettier --write --ignore-unknown',
 
   // Lint and type-check TypeScript files
   '*.{ts,tsx}': [
     'eslint --fix',
-    () => 'tsc --noEmit', // Run type check on all files
+    'tsc --noEmit', // Run type check on all files
   ],
 }
